72bcbc989917f4974a00cfbb39af92bf4c7e035b
Keagan Dickinson <keagantdickinson@gmail.com>
2022-06-01 21:24:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,5 @@ console.log(computerPlay(computerSelection))
 let playerSelection = prompt("Rock,Scissors,Paper?")
 
 function game1 (playerSelection, computerSelection) {
-   if (playerSelection == computerSelection) {
-       console.log("Tie")
-   }
+    
 }
