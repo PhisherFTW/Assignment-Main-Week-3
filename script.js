ff3567b8e457a0bbdfb8ee6166ffdf830bd57b98
Keagan Dickinson <keagantdickinson@gmail.com>
2022-06-03 21:18:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,62 @@
 let computerSelection = null;
 
 function computerPlay (computerSelection) {
-    let gamechoices = ["rock","paper","scissors"];
-    return computerSelection = gamechoices[Math.floor(Math.random() * gamechoices.length)];
+  let gamechoices = ["rock","paper","scissors"];
+  return computerSelection = gamechoices[Math.floor(Math.random() * gamechoices.length)];
 }
 let computerChoice = computerPlay(computerSelection);
 
 let playerSelection = prompt("Rock,Scissors,Paper?");
 playerSelection =playerSelection.toLocaleLowerCase();
 
-// var results = null;
 
-    // function game1(playerSelection,computerChoice, results) {
-    // if computerChoice === playerSelection) {
-    //     results = "It was a tie!";
-    // }
-    // if (playerSelection == "rock" && computerChoice == "paper") {
-    //     results = "You lost!, Paper beats Rock!";
-    // }
-    // if (playerSelection == "paper" && computerPlay(computerSelection) == "scissors") {
-    //     results = "You lost!, Scissors beats Paper";
-    // }
-    // if (playerSelection == "scissors" && computerPlay(computerSelection) == "rock") {
-    //     results = "You lost!, Rock beats Scissors!";
-    // }
-    // if (playerSelection == "paper" && computerPlay(computerSelection) == "rock") {
-    //     results = "You won!, Paper beats Rock!";
-    // }
-    // if (playerSelection == "scissors" && computerPlay(computerSelection) == "paper") {
-    //     results = "You won!, Scissors beats paper!";
-    // }
-    // if (playerSelection == "rock" && computerPlay(computerSelection) == "scissors") {
-    //     results = "You won!, Rock beats Scissors!";
-    // }
-    // console.log(results);
-// }
-// game1(playerSelection, computerSelection);
-const getResults = (playerSelection, computerChoice) => {
-    switch (playerSelection + computerChoice) {
-      case 'scissorspaper':
-      case 'rockscissors':
-      case 'paperrock':
-        console.log('You chose ' + playerSelection + ' and the computer chose ' + computerChoice + ' , YOU WIN!');
-        break
-      case 'paperscissors':
-      case 'scissorsrock':
-      case 'rockpaper':
-        console.log('You chose ' + playerSelection + ' and the computer chose ' + computerChoice + ' , YOU LOSE!');
-        break
-      case 'scissorsscissors':
-      case 'rockrock':
-      case 'paperpaper':
-        console.log('You chose ' + playerSelection + ' and the computer chose ' + computerChoice + ' , ITS A DRAW!');
-        break
-    }
+function game1(playerSelection,computerChoice) {
+  if (playerSelection === computerChoice) {
+      console.log("It was a tie!");
   }
-getResults(playerSelection,computerChoice)
+  if (playerSelection == "rock" && computerChoice == "paper") {
+      console.log("You lost!, Paper beats Rock!");
+  }
+  if (playerSelection == "paper" && computerChoice == "scissors") {
+      console.log("You lost!, Scissors beats Paper!");
+  }
+  if (playerSelection == "scissors" && computerChoice == "rock") {
+      console.log("You lost!, Rock beats Scissors!");
+  }
+  if (playerSelection == "paper" && computerChoice == "rock") {
+      console.log("You won!, Paper beats Rock!");
+  }
+  if (playerSelection == "scissors" && computerChoice == "paper") {
+      console.log("You won!, Scissors beats paper!");
+  }
+  if (playerSelection == "rock" && computerChoice == "scissors") {
+      console.log("You won!, Rock beats Scissors!");
+  }
+}
+
+game1(playerSelection, computerChoice);
 console.log(playerSelection);
-console.log(computerChoice);
+console.log(computerChoice)
+
+
+
+// const getResults = (playerSelection, computerChoice) => {
+//     switch (playerSelection + computerChoice) {
+//       case 'scissorspaper':
+//       case 'rockscissors':
+//       case 'paperrock':
+//         console.log(playerSelection + ' beats ' + computerChoice + ' , You won!');
+//         break
+//       case 'paperscissors':
+//       case 'scissorsrock':
+//       case 'rockpaper':
+//         console.log(playerSelection + ' beats ' + computerChoice + ' , You lost!');
+//         break
+//       case 'scissorsscissors':
+//       case 'rockrock':
+//       case 'paperpaper':
+//         console.log(playerSelection + ' and the computer chose ' + computerChoice + ' , It was a tie!');
+//         break
+//     }
+//   }
+// getResults(playerSelection,computerChoice)
