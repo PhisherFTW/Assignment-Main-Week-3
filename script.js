a7ba6ce50c42040ce22c28aea3dac2965d1afdd2
Keagan Dickinson <keagantdickinson@gmail.com>
2022-06-04 21:09:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,53 @@
 let computerSelection = null;
-
 function computerPlay (computerSelection) {
   let gamechoices = ["rock","paper","scissors"];
   return computerSelection = gamechoices[Math.floor(Math.random() * gamechoices.length)];
 }
-let computerChoice = computerPlay(computerSelection);
-
-let playerSelection = prompt("Rock,Scissors,Paper?");
-playerSelection =playerSelection.toLocaleLowerCase();
 
 
-function game1(playerSelection,computerChoice) {
-  if (playerSelection === computerChoice) {
-      console.log("It was a tie!");
-  }
-  if (playerSelection == "rock" && computerChoice == "paper") {
-      console.log("You lost!, Paper beats Rock!");
-  }
-  if (playerSelection == "paper" && computerChoice == "scissors") {
-      console.log("You lost!, Scissors beats Paper!");
-  }
-  if (playerSelection == "scissors" && computerChoice == "rock") {
-      console.log("You lost!, Rock beats Scissors!");
-  }
-  if (playerSelection == "paper" && computerChoice == "rock") {
-      console.log("You won!, Paper beats Rock!");
-  }
-  if (playerSelection == "scissors" && computerChoice == "paper") {
-      console.log("You won!, Scissors beats paper!");
-  }
-  if (playerSelection == "rock" && computerChoice == "scissors") {
-      console.log("You won!, Rock beats Scissors!");
-  }
-}
-
-game1(playerSelection, computerChoice);
-console.log(playerSelection);
-console.log(computerChoice)
+let playerReaction = confirm("Would you like to play the game?");
 
 
 
-// const getResults = (playerSelection, computerChoice) => {
-//     switch (playerSelection + computerChoice) {
-//       case 'scissorspaper':
-//       case 'rockscissors':
-//       case 'paperrock':
-//         console.log(playerSelection + ' beats ' + computerChoice + ' , You won!');
-//         break
-//       case 'paperscissors':
-//       case 'scissorsrock':
-//       case 'rockpaper':
-//         console.log(playerSelection + ' beats ' + computerChoice + ' , You lost!');
-//         break
-//       case 'scissorsscissors':
-//       case 'rockrock':
-//       case 'paperpaper':
-//         console.log(playerSelection + ' and the computer chose ' + computerChoice + ' , It was a tie!');
-//         break
-//     }
-//   }
-// getResults(playerSelection,computerChoice)
+function doSomething() {
+    
+    for(let i = 1; i<6; i++) {
+        let playerScore = 0;
+        let computerScore = 0;
+        let computerChoice = computerPlay(computerSelection);
+        let playerSelection =null;
+
+        playerSelection = prompt("pick one of the options: Rock,Paper,Scissors");
+        if (playerSelection === null) {
+            console.log("Why did you exit the game?");
+            return;
+        }
+        playerSelection = playerSelection.toLocaleLowerCase();
+        
+        if (playerSelection === computerChoice) {
+            console.log("It was a tie!");
+        }
+        if (playerSelection == "rock" && computerChoice == "paper") {
+            console.log("You lost!, Paper beats Rock!") + computerScore+1;
+        }
+        if (playerSelection == "paper" && computerChoice == "scissors") {
+            console.log("You lost!, Scissors beats Paper!") + (computerScore+1);
+        }
+        if (playerSelection == "scissors" && computerChoice == "rock") {
+            console.log("You lost!, Rock beats Scissors!") + (computerScore+1);
+        }
+        if (playerSelection == "paper" && computerChoice == "rock") {
+            console.log("You won!, Paper beats Rock!") + (playerScore+1);
+        }
+        if (playerSelection == "scissors" && computerChoice == "paper") {
+            console.log("You won!, Scissors beats paper!") + (playerScore+1);
+        }
+        if (playerSelection == "rock" && computerChoice == "scissors") {
+            console.log("You won!, Rock beats Scissors!") + (playerScore+1);
+        }
+
+        
+        console.log("Round " + i + " computer's choice = " + computerChoice + " Your choice = " + playerSelection);
+    }
+    }
+doSomething();
